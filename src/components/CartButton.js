a41767a3cac26def20eaed02689091f9932148b7
Ana Laura <analauraalves25@gmail.com>
2021-07-12 19:21:38 -0300

    adiciona css do botao e botao home no cart

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import icon from '../icon/shopping-cart.png';
+import '../styles/CartButton.css';
 
 class CartButton extends Component {
   constructor(props) {
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ShoppingCart extends Component {
   render() {
     return (
       <div data-testid="shopping-cart-empty-message">
         Seu carrinho está vazio
+        <button type="button"><Link to="/">HOME</Link></button>
       </div>
     );
   }
