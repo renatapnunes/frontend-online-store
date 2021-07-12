6d3f67ce93120cca92d028a73c1b9c39d810db2c
Ana Laura <analauraalves25@gmail.com>
2021-07-12 16:31:51 -0300

    resolve lint

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -3,28 +3,26 @@ import React, { Component } from 'react';
 class SearchInput extends Component {
   constructor() {
     super();
-    
+
     this.state = {
-      value: " ",
-    }
+      value: ' ',
+    };
   }
 
   render() {
     const { value } = this.state;
     return (
-      <>
-        <label 
-          htmlFor="search-input"
-          data-testid="home-initial-message"
-        >
-          <input 
-            id="serach-input"
-            type="text"
-            value={ value }
-          />
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </label>
-      </>
+      <label
+        htmlFor="search-input"
+        data-testid="home-initial-message"
+      >
+        <input
+          id="serach-input"
+          type="text"
+          value={ value }
+        />
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </label>
     );
   }
 }
