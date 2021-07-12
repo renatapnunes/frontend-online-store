7274ef2189a11faea494443e5804b8f9876a3db5
Ana Laura <analauraalves25@gmail.com>
2021-07-12 16:20:36 -0300

    requisito-2-grupo-laurinha

---
+++ src/components/SearchInput.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class SearchInput extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      value: " ",
+    }
+  }
+
+  render() {
+    const { value } = this.state;
+    return (
+      <>
+        <label 
+          htmlFor="search-input"
+          data-testid="home-initial-message"
+        >
+          <input 
+            id="serach-input"
+            type="text"
+            value={ value }
+          />
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </label>
+      </>
+    );
+  }
+}
+
+export default SearchInput;
