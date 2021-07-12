08ae10ccd0c4a46b27242532176eb8e91314050f
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-07-12 17:07:37 -0300

    requisito-1-grupo-renatinha review

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import * as api from './services/api';
 class App extends Component {
   componentDidMount() {
     api.getCategories()
-      .then((promise) => { console.log(promise); });
+      .then((promise) => console.log(promise));
     api.getProductsFromCategoryAndQuery('MLB5672')
       .then((categoria) => console.log(categoria));
   }
